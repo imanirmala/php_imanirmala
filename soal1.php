6d671d092416f3269fde76600915a6a22df3dffe
imanirmala <imanirmalasetiawan@gmail.com>
2022-11-11 16:25:25 +0700

    soal1.php

---
+++ soal1.php
@@ -0,0 +1,16 @@
+<?php
+
+$jml = $_GET['jml'];
+echo "<table border=1>\n";
+for ($a = $jml; $a > 0; $a--)
+{
+  echo "<tr>\n";
+  for ($b = $a; $b > 0; $b--)
+  {
+    echo "<td>$b</td>";
+  }
+  echo "</tr>\n";
+}
+echo "</table>";
+
+?>
