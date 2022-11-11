0bbcbf6c8cc3dd2c44e4f489339344508d65532d
imanirmala <imanirmalasetiawan@gmail.com>
2022-11-11 19:08:20 +0700

    fixing soal no 1

--- soal1.php
+++ soal1.php
@@ -1,9 +1,23 @@
 <?php
 
 $jml = $_GET['jml'];
+$tampungan = 0;
 echo "<table border=1>\n";
 for ($a = $jml; $a > 0; $a--)
 {
+  echo "<tr>\n";
+  
+  for ($b = $a; $b > 0; $b--)
+  {
+    $tampungan = + $b;
+    if($b == 1){
+      echo "$tampungan";
+    }
+    
+  }
+  echo "</tr>\n";
+
+
   echo "<tr>\n";
   for ($b = $a; $b > 0; $b--)
   {
